5f018c59c1d75a4781d9d82b37ee68c028fc39db
Jane Philipps <jane.philipps@gmail.com>
2017-01-07 17:39:16 -0800

    styling links

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory, IndexLink } from 'react-router'
 
 class App extends Component {
     render() {
@@ -7,7 +7,10 @@ class App extends Component {
             <Router history={hashHistory}>
                 <Route path='/' component={Container}>
                     <IndexRoute component={Home} />
-                    <Route path='/address' component={Address} />
+                    <Route path='/address' component={Address}>
+                        <IndexRoute component={TwitterFeed} />
+                        <Route path='instagram' component={InstagramFeed} />
+                    </Route>
                     <Route path='*' component={NotFound} />
                 </Route>
             </Router>
@@ -16,13 +19,22 @@ class App extends Component {
 }
 
 const Home = () => <h1>Hello from Home!</h1>
-const Address = () => <h1>We are located at 555 Jackson St.</h1>
+const Address = (props) => (
+    <div>
+        <br />
+        <Link to='/address'>Twitter Feed</Link>&nbsp;
+        <Link to='/address/instagram'>Instagram Feed</Link>
+        <h1>We are located at 555 Jackson St.</h1>
+        {props.children}
+    </div>
+)
 const NotFound = () => <h1>404...This page is not found!</h1>
 
 const Nav = () => (
     <div>
-        <Link to='/'>Home</Link>&nbsp;
-        <Link to='/address'>Address</Link>
+        <IndexLink activeClassName='active' to='/'>Home</IndexLink>&nbsp;
+        <IndexLink activeClassName='active' to='/address'>Address</IndexLink>&nbsp;
+        <IndexLink activeClassName='active' to='/about'>About</IndexLink>
     </div>
 )
 
@@ -32,6 +44,10 @@ const Container = (props) => (
         {props.children}
     </div>
 )
+
+const InstagramFeed = () => <h3>Instagram Feed</h3>
+const TwitterFeed = () => <h3>Twitter Feed</h3>
+
 
 
 export default App
